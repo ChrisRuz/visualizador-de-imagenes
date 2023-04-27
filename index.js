e891527627d3dd28a52294f45a1acc7f3b84032b
Chris Ruz <le17081417@merida.tecnm.mx>
2023-04-26 19:25:02 -0600

    Se quito el prompt y se agrego que lea mejor el input password para que no se muestre cuando se tecle la contraseña y se agrego un ciclo para que el usuario tenga 3 intentos

--- index.js
+++ index.js
@@ -11,11 +11,12 @@ botonMostrar.addEventListener("click", () => {
     // const imagenNombre = botonMostrar.getAttribute("data-imagen-id");
     // document.querySelector("#imagen-nombre").textContent = imagenNombre;
     const url = new URL(window.location.href);
-    url.searchParams.set('imagen', imagenId);
-    window.history.pushState({}, '', url); 
+    url.searchParams.set("imagen", imagenId);
+    window.history.pushState({}, "", url);
     imagen1.src = `https://sisivey.com/apiobraivey/imagenes/${imagenId}.JPG`;
-    imagen2.src = `https://sisivey.com/apiobraivey/imagenes/${imagenId + ".2"
-      }.JPG`;
+    imagen2.src = `https://sisivey.com/apiobraivey/imagenes/${
+      imagenId + ".2"
+    }.JPG`;
     imagenTitulo.textContent = imagenId; // Asignar el valor dinámico al h2
     modal.style.display = "block";
   } else {
@@ -23,24 +24,37 @@ botonMostrar.addEventListener("click", () => {
   }
 });
 
-const imagenId = new URLSearchParams(window.location.search).get('imagen');
+const imagenId = new URLSearchParams(window.location.search).get("imagen");
 if (imagenId) {
-  const password = prompt("Ingrese la contraseña:");
-  if (password === "contraseña") {
-    const botonMostrar = document.querySelector("#descargar");
-    const modal = document.querySelector("#modal");
-    const imagen1 = modal.querySelector("#imagen1");
-    const imagen2 = modal.querySelector("#imagen2");
-    const imagenTitulo = modal.querySelector("#imagen-titulo");
-    botonMostrar.dataset.imagenId = imagenId;
-    imagen1.src = `https://sisivey.com/apiobraivey/imagenes/${imagenId}.JPG`;
-    imagen2.src = `https://sisivey.com/apiobraivey/imagenes/${imagenId + ".2"}.JPG`;
-    imagenTitulo.textContent = imagenId;
-    modal.style.display = "block";
-  } else {
-    alert("Contraseña incorrecta");
-    window.history.pushState({}, '', '/');
-  }
+  const botonMostrar = document.querySelector("#descargar");
+  const passwordInput = document.querySelector("#password");
+  let attempts = 0; // Contador de intentos
+  botonMostrar.addEventListener("click", () => {
+    const password = passwordInput.value;
+    if (password === "contraseña") {
+      const modal = document.querySelector("#modal");
+      const imagen1 = modal.querySelector("#imagen1");
+      const imagen2 = modal.querySelector("#imagen2");
+      const imagenTitulo = modal.querySelector("#imagen-titulo");
+      botonMostrar.dataset.imagenId = imagenId;
+      imagen1.src = `https://sisivey.com/apiobraivey/imagenes/${imagenId}.JPG`;
+      imagen2.src = `https://sisivey.com/apiobraivey/imagenes/${
+        imagenId + ".2"
+      }.JPG`;
+      imagenTitulo.textContent = imagenId;
+      modal.style.display = "block";
+    } else {
+      attempts++; // Incrementar el contador de intentos
+      if (attempts === 1) {
+        alert("Le quedan 2 intentos.");
+      } else if (attempts === 2) {
+        alert("Le queda 1 intento.");
+      } else {
+        alert("Intentos excesivos. Intente nuevamente más tarde.");
+        window.history.pushState({}, "", "/");
+      }
+    }
+  });
 }
 
 const botonCerrar = document.querySelector("#cerrar");
@@ -52,42 +66,3 @@ botonCerrar.addEventListener("click", () => {
   imagen1.src = "";
   imagen2.src = "";
 });
-
-/* 
-const botonMostrar = document.querySelector("#descargar");
-botonMostrar.addEventListener("click", () => {
-  let intentos = 3;
-  const imagenId = botonMostrar.dataset.imagenId;
-  let password;
-
-  while (intentos > 0) {
-    password = prompt("Ingrese la contraseña:");
-    if (password === "contraseña") {
-      break;
-    } else {
-      intentos--;
-      if (intentos > 0) {
-        alert(`Contraseña incorrecta. Le quedan ${intentos} intentos.`);
-      } else {
-        alert("Intentos excesivos.");
-        return;
-      }
-    }
-  }
-
-  const modal = document.querySelector("#modal");
-  const imagen1 = modal.querySelector("#imagen1");
-  const imagen2 = modal.querySelector("#imagen2");
-  const imagenTitulo = modal.querySelector("#imagen-titulo");
-  
-  imagen1.src = `https://sisivey.com/apiobraivey/imagenes/${imagenId}.JPG`;
-  imagen2.src = `https://sisivey.com/apiobraivey/imagenes/${imagenId + ".2"}.JPG`;
-  imagenTitulo.textContent = imagenId;
-  modal.style.display = "block";
-
-  const urlParams = new URLSearchParams(window.location.search);
-  urlParams.set('imagen', imagenId);
-  const newUrl = window.location.origin + window.location.pathname + '?' + urlParams.toString();
-  window.history.replaceState({}, '', newUrl);
-});
-*/
